50f8e35371041805b8d749ae988c0b61ab335c9f
Josh Gum <josh.gum@oregonstate.edu>
2016-12-13 15:44:11 -0800

    fixes kiosk#show route and data to tie frontend to backend

--- app/controllers/kiosk_controller.rb
+++ app/controllers/kiosk_controller.rb
@@ -1,48 +1,11 @@
 class KioskController < ApplicationController
   layout "react_application"
-  before_action :set_params
 
   def index
   end
 
   def show
-    new_slide = @slides.last.clone
-    new_slide[:id] = DateTime.now.to_time.to_i
-    new_slide[:caption] = new_slide[:id]
-    #new_slide[:image_url] = "/uploads/random.png"
-    @slides << new_slide
-  end
-
-  private
-
-  def set_params
-    @kiosk_type ||= params[:id]
-    @slide_type = "bogus"
-    # TODO: replace this with current slides for a specific kiosk type
-    @slides = [
-        {
-            id: 1,
-            image_url: "/images/default_slide_1.png",
-            expires_at: "2020-12-31T23:59:59Z",
-            created_at: "2000-12-31T11:59:59Z",
-            updated_at: "2000-12-31T23:59:59Z",
-            title: "Slide.title",
-            caption: "Slide.caption:0",
-            slide_type: "touch",
-            kiosk: "Kiosk.name"
-        },{
-            id: 2,
-            image_url: "/images/default_slide_2.png",
-            expires_at: "2020-12-31T23:59:59Z",
-            created_at: "2000-12-31T11:59:59Z",
-            updated_at: "2000-12-31T23:59:59Z",
-            title: "Slide.title",
-            caption: "Slide.caption:1",
-            slide_type: "touch",
-            kiosk: "Kiosk.name"
-        }
-    ]
-
-    end
+    @kiosk = Kiosk.find_by(name: params[:id])
+    @slides = @kiosk.slides
   end
 end
