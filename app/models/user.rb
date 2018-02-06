3d66554453686cf207900867aa91d9428a3ee71a
luisgreg99 <luisram90_101@hotmail.com>
2018-02-06 11:24:55 -0800

    upated spec for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
   def cas_extra_attributes=(extra_attributes)
     extra_attributes.each do |name, value|
       case name.to_sym
-        # when :fullname
-        #   self.fullname = value
         when :email
           self.email = value
       end
