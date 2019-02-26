a3a0d8d1233a0af40aeca356a49ebec6295a5fc0
Josh Gum <revgum@gmail.com>
2019-02-26 15:44:54 -0800

    adds basic donor coverflow kiosk ui

---
+++ app/assets/javascripts/react/components/DonorCoverflowKiosk.js
@@ -0,0 +1,29 @@
+import { bindActionCreators } from "redux"
+import { connect } from "react-redux"
+
+import Kiosk from "./presentational/DonorCoverflow/Kiosk"
+import * as actions from "../actions"
+
+// Top level mapping of the application state to properties of component that is being
+// connected.
+export const mapStateToProps = state => {
+  return {
+    slides: state.kiosk.slides,
+    kiosk_name: state.kiosk.name,
+    kiosk_id: state.kiosk.kid,
+    restart_kiosk: state.kiosk.restart_kiosk,
+    url: state.kiosk.url,
+    google_analytics: state.kiosk.google_analytics
+  }
+}
+
+// Map the actions to the dispatcher
+export const mapDispatchToProps = dispatch => {
+  return bindActionCreators(actions, dispatch)
+}
+
+// Connect the mappings (state -> properties, and actions -> dispatch) to the application component
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Kiosk)
