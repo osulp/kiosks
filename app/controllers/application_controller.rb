777a37dcdb574f1fc4186545b7042c532e182228
Ryan Ordway <1882223+decimalator@users.noreply.github.com>
2019-06-10 13:29:31 -0700

    Added Honeycomb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,11 @@
 # Application controller base
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  if %w[production staging development].include? Rails.env
+    def append_info_to_payload(payload)
+      super(payload)
+      Rack::Honeycomb.add_field(request.env, 'classname', self.class.name)
+    end
+  end
 end
