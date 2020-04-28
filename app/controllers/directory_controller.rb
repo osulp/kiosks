2183e3facbb1d7c6f59f3a6127b709c0d170be42
straleyb <straleyb@onid.oregonstate.edu>
2020-04-28 07:34:33 -0700

    add controller to route to directory pages

---
+++ app/controllers/directory_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Controller for routing the Directory Pages
+class DirectoryController < ApplicationController 
+  def csv_page
+  end
+
+  def iframe_page
+  end
+
+  def editor_page
+  end
+end
