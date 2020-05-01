8b70b0c4d54e42d2b841ff6360807f71645c9c1e
straleyb <straleyb@onid.oregonstate.edu>
2020-05-01 08:12:47 -0700

    Adds csv upload and refactors some directory naming best practices

---
+++ app/controllers/directories_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Controller for routing the Directory Pages
+class DirectoriesController < ApplicationController
+  def csv_page; end
+
+  def iframe_page; end
+
+  def editor_page
+    @directory = Directory.new
+  end
+
+  def show
+    @directory = Directory.new
+  end
+
+  def new
+    @directory = Directory.new
+  end
+
+  def index
+    @directories = Directory.all
+  end
+
+  def create
+    Directory.import(params[:directory][:file])
+    flash[:notice] = "Directory uploaded successfully"
+    redirect_to directories_path #=> or where you want
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_162347) do
+ActiveRecord::Schema.define(version: 2020_05_01_142653) do
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string "data_file_name", null: false
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 2020_04_30_162347) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "title"
+    t.string "phone_number"
   end
 
   create_table "kiosk_layouts", force: :cascade do |t|
