2a6b06bdbc438d27c28b4096292ca615057b8630
straleyb <straleyb@onid.oregonstate.edu>
2020-05-06 08:28:46 -0700

    remove engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  mount Ckeditor::Engine => '/ckeditor'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :kiosk_layouts
   resources :date_ranges
