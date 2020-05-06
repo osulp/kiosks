1b2770eec153e889313e44e4a5197fbdf8e095ad
Greg Luis Ramirez <gregorio.luisramirez@oregonstate.edu>
2020-05-05 17:34:20 -0700

    attach 4th floor image to admin kiosk

--- app/javascript/react/components/presentational/Admin/MenuContent.js
+++ app/javascript/react/components/presentational/Admin/MenuContent.js
@@ -2,10 +2,11 @@ import React, { Component } from "react"
 import PropTypes from "prop-types"
 
 // render tab content
-const renderMenuContent = (index) => {
+const renderMenuContent = (props) => {
+  let index = props.selectedMenuItem
   switch(index) {
     case 1:
-      return <MapContent />
+      return <MapContent {...props} />
     case 2:
       return <LibraryDirectoryContent />
     case 3:
@@ -20,15 +21,18 @@ const renderMenuContent = (index) => {
 const MenuContent = props => {
   return (
     <div className="map-content content-component">
-      {renderMenuContent(props.selectedMenuItem)}
+      {renderMenuContent(props)}
     </div>
   )
 }
 
 // TODO: We could refactor functions below and extract menu properties (content title, tab title, etc) from a yml file instead of loading from const variables
 const MapContent = props => {
+  let floor_4_url = props.maps[6].image_url
   return (
-    <h1>Map Content</h1>
+    <div className="map-content">
+      <div style={{ backgroundImage: `url("${floor_4_url}")` }}>&nbsp;</div>
+    </div>
   )
 }
 const LibraryDirectoryContent = props => {
@@ -50,7 +54,8 @@ const EcampusDirectoryContent = props => {
 }
 
 MenuContent.propTypes = {
-  selectedMenuItem: PropTypes.number.isRequired
+  selectedMenuItem: PropTypes.number.isRequired,
+  maps: PropTypes.array
 }
 
 export default MenuContent
