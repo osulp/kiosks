75bfb2b3b803e4a19172fbf0b0c7bb4882eb70a9
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2021-04-26 12:23:59 -0700

    update packages (#485)
    * update packages
    
    * update gems
    
    * rubocop updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render partial: 'slides/' + association.to_s.singularize + '_fields', locals: { f: builder }
+      render partial: "slides/#{association.to_s.singularize}_fields", locals: { f: builder }
     end
     link_to(name, '#', class: 'add_fields btn btn-success', data: { id: id, fields: fields.delete("\n") })
   end
@@ -15,7 +15,7 @@ module ApplicationHelper
     new_object = f.object.send(association).klass.new
     id = new_object.object_id
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      render partial: 'slides/' + association.to_s.singularize + '_fields', locals: { f: builder }
+      render partial: "slides/#{association.to_s.singularize}_fields", locals: { f: builder }
     end
 
     # this link is customized given the id of the uploaded file (fid) and the newly
@@ -23,7 +23,7 @@ module ApplicationHelper
     # relationship
     link_to(name, '#', class: 'add_fields btn btn-success', data: {
               id: id,
-              fields: fields.delete("\n").gsub('slide_date_ranges_attributes', 'collection_slides_attributes_' + fid + '_date_ranges_attributes').gsub('slide[date_ranges_attributes]', 'collection[slides_attributes][' + fid + '][date_ranges_attributes]')
+              fields: fields.delete("\n").gsub('slide_date_ranges_attributes', "collection_slides_attributes_#{fid}_date_ranges_attributes").gsub('slide[date_ranges_attributes]', "collection[slides_attributes][#{fid}][date_ranges_attributes]")
             })
   end
 end
