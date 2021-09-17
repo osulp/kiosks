469220bafd0df7b6eb452f3e11acae42cf3bb153
straleyb <straleyb@onid.oregonstate.edu>
2021-09-17 12:05:02 -0700

    use example for now

--- config/initializers/application_config.rb
+++ config/initializers/application_config.rb
@@ -1,1 +1,1 @@
-APPLICATION_CONFIG = YAML.load(ERB.new(File.read("#{Rails.root}/config/application_config.yml.erb")).result)
+APPLICATION_CONFIG = YAML.load(ERB.new(File.read("#{Rails.root}/config/application_config.example.yml.erb")).result)
