014aa345df31ff4df89a655a217fc43f18ee1e7d
Brandon R. Straley <straleyb@onid.oregonstate.edu>
2022-06-06 15:23:55 -0700

    Update Header.js

--- app/javascript/react/components/presentational/Circulation/Header.js
+++ app/javascript/react/components/presentational/Circulation/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
    * @private
    */
   _hoursToday() {
-    if (Object.values(this.props.hours).length == 0) {
+    if (Object.values(this.props.hours).length == 0 || Object.values(this.props.hours) == undefined) {
       return ""
     } else {
       let today = Object.values(this.props.hours)[0]
