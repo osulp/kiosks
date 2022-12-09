ad67761c78ab342282ff20b8537abc6fb234c890
straleyb <straleyb@onid.oregonstate.edu>
2022-12-09 12:18:26 -0800

    update to use proper prop

--- app/javascript/react/components/presentational/Touch/Header.js
+++ app/javascript/react/components/presentational/Touch/Header.js
@@ -95,7 +95,7 @@ class Header extends Component {
    * @private
    */
   _hoursToday() {
-    if (this.props.todays_hours == null || Object.values(this.props.hours).length == 0 || Object.values(this.props.hours) == undefined) {
+    if (this.props.hours == null || Object.values(this.props.hours).length == 0 || Object.values(this.props.hours) == undefined) {
       return ""
     } else {
       let today = Object.values(this.props.hours)[0]
